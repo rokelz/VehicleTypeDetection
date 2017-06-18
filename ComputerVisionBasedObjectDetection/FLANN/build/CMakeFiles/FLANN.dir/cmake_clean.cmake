d3f341f718e861a4f969c65f61822fd7a010b3ea
Ruwini Opatha <ruwiniopatha@gmail.com>
2017-06-18 11:59:58 +0530

    Add files via upload

---
+++ ComputerVisionBasedObjectDetection/FLANN/build/CMakeFiles/FLANN.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FLANN.dir/demo.cpp.o"
+  "FLANN.pdb"
+  "FLANN"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FLANN.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ComputerVisionBasedObjectDetection/HaarsCascade/build/CMakeFiles/HaarsCascade.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HaarsCascade.dir/demo.cpp.o"
+  "HaarsCascade.pdb"
+  "HaarsCascade"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HaarsCascade.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
