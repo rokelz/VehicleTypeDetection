df516ea075bff4a830e4707b6418177bb1d48e61
Ruwini Opatha <ruwiniopatha@gmail.com>
2017-06-19 00:06:11 +0530

    Add files via upload

---
+++ ComputerVisionBasedObjectDetection/HarisCorner/build/CMakeFiles/HarisCorner.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HarisCorner.dir/demo.cpp.o"
+  "HarisCorner.pdb"
+  "HarisCorner"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HarisCorner.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
